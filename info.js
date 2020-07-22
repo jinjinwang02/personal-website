e1c22a3a174b8091b953d099c017031d928f151c
Jinjin Wang <jinjinwang@MacBook-Pro.local>
2020-07-22 10:27:53 +0100

    update info and cv

--- info.js
+++ info.js
@@ -1,32 +1,33 @@
 //INFO FADE IN
 const faders = document.querySelectorAll(".fade-in-and-rise");
 const appearOptions = {
-    thresohld: 1
-}
+  thresohld: 1,
+};
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add("appear");
-            appearOnScroll.unobserve(entry.target);
-        }
-    })
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
 }, appearOptions);
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-})
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
 
 //INFO SPLIT SCREEN
 function splitScroll() {
-    const controller = new ScrollMagic.Controller();
+  const controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        duration: 400,
-        triggerElement: ".info__heading",
-        triggerHook: 0,
-    }).setPin(".info__heading")
-        // .addIndicators()
-        .addTo(controller);
+  new ScrollMagic.Scene({
+    duration: 500,
+    triggerElement: ".info__heading",
+    triggerHook: 0,
+  })
+    .setPin(".info__heading")
+    // .addIndicators()
+    .addTo(controller);
 }
 splitScroll();
